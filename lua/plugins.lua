e5e7eb3355b72c832c8c8973687ae9e99722b10b
Arvid Warnecke <madhatter@nostalgix.org>
2024-03-01 16:18:49 +0100

    [feature] Install some treesitter parsers by default.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -16,5 +16,18 @@ require("lazy").setup({
   "wincent/command-t",
   "rebelot/kanagawa.nvim",
   "nvim-lualine/lualine.nvim",
-  "nvim-treesitter/nvim-treesitter",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "ruby", "python", "go" },
+
+            auto_install = true,
+
+            highlight = {
+                enable = true
+            },
+        })
+        end,
+  },
 })
