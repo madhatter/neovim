cb437e4223fd4b673578f1627842311e5f07311a
Arvid Warnecke <madhatter@nostalgix.org>
2024-03-06 20:20:52 +0100

    [refactor] Use more files for plugin configuration.

--- lua/plugins/default.lua
+++ lua/plugins/default.lua
@@ -1,23 +1,6 @@
 return {
-	-- Themes --
-	"rebelot/kanagawa.nvim",
-
 	-- Productivity --
 	"nvim-lualine/lualine.nvim",
-	{
-		"nvim-treesitter/nvim-treesitter",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "ruby", "python", "go" },
-
-				auto_install = true,
-
-				highlight = {
-					enable = true,
-				},
-			})
-		end,
-	},
 	{
 		"ibhagwan/fzf-lua",
 		-- optional for icon support
@@ -31,14 +14,4 @@ return {
 		"christoomey/vim-tmux-navigator",
 		lazy = false,
 	},
-
-	-- Development --
-	"lewis6991/gitsigns.nvim",
-	"hrsh7th/cmp-nvim-lsp",
-	"hrsh7th/nvim-cmp",
-	"neovim/nvim-lspconfig",
-	"onsails/lspkind-nvim",
-	"williamboman/mason.nvim",
-	"williamboman/mason-lspconfig.nvim",
-	"L3MON4D3/LuaSnip",
 }
---
+++ lua/plugins/nvim-cmp.lua
@@ -0,0 +1,9 @@
+return {
+	"hrsh7th/cmp-nvim-lsp",
+	"hrsh7th/nvim-cmp",
+	"neovim/nvim-lspconfig",
+	"onsails/lspkind-nvim",
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	"L3MON4D3/LuaSnip",
+}
