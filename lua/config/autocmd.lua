0f910168dca3abb88c60276e05ce01f5803c6e46
Arvid Warnecke <madhatter@nostalgix.org>
2024-03-07 21:30:45 +0100

    [configuration] More autocmd and updated lazy json.

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -1,13 +1,24 @@
 -- Set textwidth for markdown files
 vim.api.nvim_create_autocmd(
-    {
-        "BufNewFile",
-        "BufRead",
-    },
-    {
-        pattern = "*.mdown,*.md",
-        callback = function()
-            vim.opt.textwidth = 80
-        end
-    }
+  {
+    "BufNewFile",
+    "BufRead",
+  },
+  {
+    pattern = "*.mdown,*.md",
+    callback = function()
+      vim.opt.textwidth = 80
+    end
+  }
 )
+
+-- Format augroup/autocomd
+local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+   require('go.format').goimport()
+  end,
+  group = format_sync_grp,
+})
+
