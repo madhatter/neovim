26fcf951d4708238ec6f8237b060931fafdaf401
Arvid Warnecke <madhatter@nostalgix.org>
2024-03-07 21:30:04 +0100

    [feature] Add cmp-buffer plugin for everything without LSP in the buffer and cellular-automation.

--- lua/plugins/default.lua
+++ lua/plugins/default.lua
@@ -1,17 +1,18 @@
 return {
-	-- Productivity --
-	"nvim-lualine/lualine.nvim",
-	{
-		"ibhagwan/fzf-lua",
-		-- optional for icon support
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			-- calling `setup` is optional for customization
-			require("fzf-lua").setup({})
-		end,
-	},
-	{
-		"christoomey/vim-tmux-navigator",
-		lazy = false,
-	},
+  -- Productivity --
+  "nvim-lualine/lualine.nvim",
+  {
+    "ibhagwan/fzf-lua",
+    -- optional for icon support
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      -- calling `setup` is optional for customization
+      require("fzf-lua").setup({})
+    end,
+  },
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  },
+  "eandrju/cellular-automaton.nvim",
 }
--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,9 +1,10 @@
 return {
-	"hrsh7th/cmp-nvim-lsp",
-	"hrsh7th/nvim-cmp",
-	"neovim/nvim-lspconfig",
-	"onsails/lspkind-nvim",
-	"williamboman/mason.nvim",
-	"williamboman/mason-lspconfig.nvim",
-	"L3MON4D3/LuaSnip",
+  "hrsh7th/nvim-cmp",
+  "hrsh7th/cmp-nvim-lsp",
+  "hrsh7th/cmp-buffer",
+  "neovim/nvim-lspconfig",
+  "onsails/lspkind-nvim",
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
+  "L3MON4D3/LuaSnip",
 }
