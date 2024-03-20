a878c9645ded7299d9465ad34b652208613bf903
Arvid Warnecke <madhatter@nostalgix.org>
2024-03-20 16:44:38 +0100

    [config] Enable TSUpdate in treesitter configuration.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,28 +1,29 @@
 return {
 	{
-		"nvim-treesitter/nvim-treesitter",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = {
-                    "c",
-                    "lua",
-                    "vim",
-                    "vimdoc",
-                    "query",
-                    "ruby",
-                    "python",
-                    "go",
-                    "terraform",
-                    "yaml",
-                    "regex"
-                },
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+                          "c",
+                          "lua",
+                          "vim",
+                          "vimdoc",
+                          "query",
+                          "ruby",
+                          "python",
+                          "go",
+                          "terraform",
+                          "yaml",
+                          "regex"
+        },
 
-				auto_install = true,
+        auto_install = true,
 
-				highlight = {
-					enable = true,
-				},
-			})
-		end,
-	},
+        highlight = {
+          enable = true,
+        },
+        build = ":TSUpdate",
+      })
+    end,
+  },
 }
