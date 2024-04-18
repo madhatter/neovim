efe5a1bce8d3b6fbc2d573a420b243c541704337
Arvid Warnecke <madhatter@nostalgix.org>
2024-04-18 09:41:40 +0200

    [feature] Add blame.nvim plugin. Fugitive blame style.

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,35 +1,43 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  config = function()
-    require("gitsigns").setup({
-      signs = {
-        add          = { text = '+' },
-        change       = { text = '│' },
-        delete       = { text = '_' },
-        topdelete    = { text = '‾' },
-        changedelete = { text = '~' },
-        untracked    = { text = '┆' },
-      },
-      on_attach = function(bufnr)
-        local gs = package.loaded.gitsigns
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup({
+        signs = {
+          add          = { text = '+' },
+          change       = { text = '│' },
+          delete       = { text = '_' },
+          topdelete    = { text = '‾' },
+          changedelete = { text = '~' },
+          untracked    = { text = '┆' },
+        },
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
 
-        local function map(mode, l, r, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          vim.keymap.set(mode, l, r, opts)
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+
+          map('n', '<leader>gs', gs.stage_hunk)
+          map('n', '<leader>gr', gs.reset_hunk)
+          map('n', '<leader>gS', gs.stage_buffer)
+          map('n', '<leader>gu', gs.undo_stage_hunk)
+          map('n', '<leader>gp', gs.preview_hunk)
+          map('n', '<leader>gb', function() gs.blame_line{full=true} end)
+          map('n', '<leader>tb', gs.toggle_current_line_blame)
+          map('n', '<leader>gd', gs.diffthis)
+          map('n', '<leader>gD', function() gs.diffthis('~') end)
+          map('n', '<leader>td', gs.toggle_deleted)
         end
-
-        map('n', '<leader>gs', gs.stage_hunk)
-        map('n', '<leader>gr', gs.reset_hunk)
-        map('n', '<leader>gS', gs.stage_buffer)
-        map('n', '<leader>gu', gs.undo_stage_hunk)
-        map('n', '<leader>gp', gs.preview_hunk)
-        map('n', '<leader>gb', function() gs.blame_line{full=true} end)
-        map('n', '<leader>tb', gs.toggle_current_line_blame)
-        map('n', '<leader>gd', gs.diffthis)
-        map('n', '<leader>gD', function() gs.diffthis('~') end)
-        map('n', '<leader>td', gs.toggle_deleted)
-      end
-    })
-  end
+      })
+    end
+  },
+  {
+    "FabijanZulj/blame.nvim",
+    config = function()
+      require("blame").setup()
+    end
+  }
 }
