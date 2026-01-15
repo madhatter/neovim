b9c4e6698c360dd451606bb18b3325a494e97578
Arvid Warnecke <madhatter@nostalgix.org>
2026-01-15 10:58:21 +0100

    Eliminated references to CopilotChat.actions and related functions which are no longer present in the latest CopilotChat.nvim.

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -102,24 +102,6 @@ return {
     end,
     event = "VeryLazy",
     keys = {
-      -- Show help actions with telescope
-      {
-        "<leader>cch",
-        function()
-          local actions = require("CopilotChat.actions")
-          require("CopilotChat.integrations.telescope").pick(actions.help_actions())
-        end,
-        desc = "CopilotChat - Help actions",
-      },
-      -- Show prompts actions with telescope
-      {
-        "<leader>ccp",
-        function()
-          local actions = require("CopilotChat.actions")
-          require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
-        end,
-        desc = "CopilotChat - Prompt actions",
-      },
       -- Code related commands
       { "<leader>cce", "<cmd>CopilotChatExplain<cr>",       desc = "CopilotChat - Explain code" },
       { "<leader>cct", "<cmd>CopilotChatTests<cr>",         desc = "CopilotChat - Generate tests" },
@@ -155,11 +137,6 @@ return {
         "<leader>ccm",
         "<cmd>CopilotChatCommit<cr>",
         desc = "CopilotChat - Generate commit message for all changes",
-      },
-      {
-        "<leader>ccM",
-        "<cmd>CopilotChatCommitStaged<cr>",
-        desc = "CopilotChat - Generate commit message for staged changes",
       },
       -- Quick chat with Copilot
       {
