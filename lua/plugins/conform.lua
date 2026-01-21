12ebb7edd721a2dfae36f5561959123eb2ba5ca2
Arvid Warnecke <madhatter@nostalgix.org>
2026-01-21 08:42:29 +0100

    [config] Update conform.lua file for keybindings and more formatters.

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,20 +1,41 @@
 return {
-  {
-    'stevearc/conform.nvim',
-    opts = {},
-    config = function()
-    require('conform').setup({
-      formatters_by_ft = {
-        lua = { "stylua" },
-        -- Conform will run multiple formatters sequentially
-        python = { "isort", "black" },
-        -- You can customize some of the format options for the filetype (:help conform.format)
-        rust = { "rustfmt", lsp_format = "fallback" },
-        -- Conform will run the first available formatter
-        javascript = { "prettierd", "prettier", stop_after_first = true },
-        go = { 'gofmt' },
-      },
-    })
-  end,
-  },
+	{
+		"stevearc/conform.nvim",
+		-- Lazy loading: when the command :ConformInfo is used
+		cmd = { "ConformInfo" },
+		-- Or when hitting the format keybinding
+		keys = {
+			{
+				"<leader>F",
+				function()
+					require("conform").format({
+						async = true,
+						lsp_fallback = true,
+					})
+				end,
+				mode = { "n", "v" },
+				desc = "[F]ormat buffer",
+			},
+		},
+		opts = {
+			formatters_by_ft = {
+				-- Define which formatters to use for specific filetypes
+				lua = { "stylua" },
+				-- Conform will run multiple formatters sequentially
+				python = { "isort", "black" },
+				-- You can customize some of the format options for the filetype (:help conform.format)
+				rust = { "rustfmt", lsp_format = "fallback" },
+				-- Conform will run the first available formatter
+				javascript = { "prettierd", "prettier", stop_after_first = true },
+				go = { "gofmt" },
+				-- Use the "*" filetype to run formatters on all files
+				-- "trim_whitespace" removes trailing whitespace
+				["*"] = { "trim_whitespace" },
+			},
+			default_format_opts = {
+				lsp_fallback = true,
+			},
+			format_on_save = nil,
+		},
+	},
 }
