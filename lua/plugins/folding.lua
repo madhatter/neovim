10499c0a280b10d6bd49290cba1de91478f7367a
Arvid Warnecke <madhatter@nostalgix.org>
2026-01-25 00:39:16 +0100

    [plugin] Add smoth-resize.nvim to resize windows continuously.

--- lua/plugins/folding.lua
+++ lua/plugins/folding.lua
@@ -1,13 +1,17 @@
 return {
-  {
-    "kevinhwang91/nvim-ufo",
-    dependencies = { "kevinhwang91/promise-async" },
-    config = function()
-      require('ufo').setup({
-        provider_selector = function(bufnr, filetype, buftype)
-          return {'treesitter', 'indent'}
-        end
-      })
-    end
-  }
+	{
+		"kevinhwang91/nvim-ufo",
+		dependencies = { "kevinhwang91/promise-async" },
+		config = function()
+			require("ufo").setup({
+				provider_selector = function(bufnr, filetype, buftype)
+					return { "treesitter", "indent" }
+				end,
+			})
+		end,
+	},
+	{
+		"aronjohanns/smooth-resize.nvim",
+		opts = {},
+	},
 }
