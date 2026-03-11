2ba89fb770164adac3be6a64b6058caf2b86376e
Arvid Warnecke <madhatter@nostalgix.org>
2026-03-11 10:09:02 +0100

    [conform] Add prettier for html and css.

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -31,6 +31,8 @@ return {
 				-- You can customize some of the format options for the filetype (:help conform.format)
 				rust = { "rustfmt", lsp_format = "fallback" },
 				-- Conform will run the first available formatter
+				html = { "prettier" },
+				css = { "prettier" },
 				javascript = { "prettierd", "prettier", stop_after_first = true },
 				kotlin = { "ktfmt" },
 				go = { "gofmt" },
