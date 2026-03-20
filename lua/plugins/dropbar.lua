54e9b62916849430f44c0c12fbcc4a14d80446bd
Arvid Warnecke <madhatter@nostalgix.org>
2026-03-20 12:06:09 +0100

    [theme] Refine dropbar: disable in terminals, fix hover highlights
    - Disable winbar in terminal buffers via bar.enable callback
    - Add TermOpen autocmd as fallback for plugin-managed terminals
    - Fix dropdown hover colors (DropBarMenuHoverEntry/Icon/CurrentContext)
    - Remove bg from DropBarKind* groups to avoid menu background conflicts
    - Restore missing DropBarKindDir highlight

--- lua/plugins/dropbar.lua
+++ lua/plugins/dropbar.lua
@@ -4,6 +4,12 @@ return {
         dependencies = { "nvim-tree/nvim-web-devicons" },
         config = function()
             require("dropbar").setup({
+                bar = {
+                    enable = function(buf, win, _)
+                        return vim.api.nvim_buf_get_option(buf, "buftype") ~= "terminal"
+                            and vim.api.nvim_win_get_config(win).relative == ""
+                    end,
+                },
                 icons = {
                     ui = {
                         bar = {
@@ -15,12 +21,17 @@ return {
 
             local p = require("config.palette")
             vim.api.nvim_set_hl(0, "DropBarIconUISeparator", { bg = p.base01, fg = p.cyan })
-            vim.api.nvim_set_hl(0, "DropBarKindFile",     { bg = p.base01, fg = p.fg })
-            vim.api.nvim_set_hl(0, "DropBarKindFunction", { bg = p.base01, fg = p.yellow })
-            vim.api.nvim_set_hl(0, "DropBarKindMethod",   { bg = p.base01, fg = p.yellow })
-            vim.api.nvim_set_hl(0, "DropBarKindClass",    { bg = p.base01, fg = p.yellow })
-            vim.api.nvim_set_hl(0, "DropBarKindModule",   { bg = p.base01, fg = p.orange })
-            vim.api.nvim_set_hl(0, "DropBarKindStruct",   { bg = p.base01, fg = p.yellow })
+            vim.api.nvim_set_hl(0, "DropBarKindDir",      { fg = p.base03 })
+            vim.api.nvim_set_hl(0, "DropBarKindFile",     { fg = p.fg })
+            vim.api.nvim_set_hl(0, "DropBarKindFunction", { fg = p.yellow })
+            vim.api.nvim_set_hl(0, "DropBarKindMethod",   { fg = p.yellow })
+            vim.api.nvim_set_hl(0, "DropBarKindClass",    { fg = p.yellow })
+            vim.api.nvim_set_hl(0, "DropBarKindModule",   { fg = p.orange })
+            vim.api.nvim_set_hl(0, "DropBarKindStruct",   { fg = p.yellow })
+
+            vim.api.nvim_set_hl(0, "DropBarMenuHoverEntry",     { bg = p.base02, fg = p.fg })
+            vim.api.nvim_set_hl(0, "DropBarMenuHoverIcon",      { bg = p.base02 })
+            vim.api.nvim_set_hl(0, "DropBarMenuCurrentContext", { bg = p.base02 })
         end,
     },
 }
